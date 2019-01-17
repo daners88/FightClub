5d53bedf0c21427d27d33133ef5b8b985d91bae2
Alex Kofford <alexkofford@gmail.com>
2019-01-17 11:27:34 -0700

    put in hp bar and bad particle effect

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Enemy : MonoBehaviour {
+
+    public float hitStrength = 6;
+    private Slider enemyHp;
+	// Use this for initialization
+	void Start () {
+        enemyHp = GameObject.Find("EnemyHpSlider").GetComponent<Slider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.name.Contains("swat"))
+        {
+            if (col.relativeVelocity.magnitude > hitStrength)
+            {
+                print("ya boi got hit");
+                gameObject.GetComponentInChildren<ParticleScript>().EmissionCaller(col);
+                enemyHp.value -= 100;
+            }
+            else
+            {
+                print("smol hit");
+                enemyHp.value -= 1;
+            }
+        }
+
+        if(enemyHp.value == 0)
+        {
+            transform.position = new Vector3(4.5f, 4.5f, -5f);
+            enemyHp.value = 1000;
+        }
+    }
+}
---
+++ Assets/Scripts/ParticleScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void EmissionCaller(Collision col)
+    {
+        transform.position = col.contacts[0].point;
+        gameObject.GetComponent<ParticleSystem>().Emit(1);
+    }
+}
