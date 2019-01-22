d33cd3428c2a8edf4425e92b5b6f979d89f43de4
daners88 <daners88@yahoo.com>
2019-01-22 13:11:37 -0700

    added bobfather to scene

--- Assets/Scripts/RefereeSystem.cs
+++ Assets/Scripts/RefereeSystem.cs
@@ -18,6 +18,9 @@ public class RefereeSystem : MonoBehaviour {
     public float p1CurrentHealth, p2CurrentHealth;
     public float p1MaxHealth, p2MaxHealth;
 
+    public float hitStrength = 6;
+    public GameObject particleHit;
+
 
     // Use this for initialization
     void Start () {
@@ -36,34 +39,58 @@ public class RefereeSystem : MonoBehaviour {
                     PlayRandomSound();
 
                     p2CurrentHealth -= 10;
+
                     
                     ChangeHealth();
                 }
             }   
         }
 
-        foreach (var weapon in player2Weapons)
-        {
-            foreach (var vulnerable in player1Vulnerables)
-            {
-                if (weapon.GetComponent<BoxCollider>().bounds.Intersects(vulnerable.GetComponent<BoxCollider>().bounds))
-                {
-                    corrineStreak++;
-                    if(corrineStreak > 3)
-                    {
-                        catty.Play();
-                    }
-                    else
-                    {
-                        PlayRandomSound();
-                    }
+        //foreach (var weapon in player2Weapons)
+        //{
+        //    foreach (var vulnerable in player1Vulnerables)
+        //    {
+        //        if (weapon.GetComponent<BoxCollider>().bounds.Intersects(vulnerable.GetComponent<BoxCollider>().bounds))
+        //        {
+        //            corrineStreak++;
+        //            if(corrineStreak > 3)
+        //            {
+        //                catty.Play();
+        //            }
+        //            else
+        //            {
+        //                PlayRandomSound();
+        //            }
 
-                    p1CurrentHealth -= 10;
+        //            p1CurrentHealth -= 10;
                     
-                    ChangeHealth();
-                }
+        //            ChangeHealth();
+        //        }
+        //    }
+        //}
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name.Contains("mixamorig"))
+        {
+            if (col.relativeVelocity.magnitude > hitStrength)
+            {
+                print("ya boi got hit");
+                particleHit.GetComponent<ParticleScript>().EmissionCaller(col);
+            }
+            else
+            {
+                print("smol hit");
             }
         }
+
+        /*
+        if(enemyHp.value == 0)
+        {
+            transform.position = new Vector3(4.5f, 4.5f, -5f);
+            enemyHp.value = 1000;
+        }*/
     }
 
     void ChangeHealth()
