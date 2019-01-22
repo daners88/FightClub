c4e113a548c0bc6105647f72209fbb44ec5d78dc
Alex Kofford <alexkofford@gmail.com>
2019-01-22 10:32:22 -0700

    Got both trackers working

---
+++ Assets/Scripts/GetCorrectController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class GetCorrectController : MonoBehaviour {
+
+    public bool rightHand = false;
+	// Use this for initialization
+	void Start () {
+        uint leftIndex = OpenVR.System.GetTrackedDeviceIndexForControllerRole(ETrackedControllerRole.LeftHand);
+        uint rightIndex = OpenVR.System.GetTrackedDeviceIndexForControllerRole(ETrackedControllerRole.RightHand);
+        uint index = leftIndex;
+        if(rightHand)
+        {
+            index = rightIndex;
+        }
+        GetComponent<SteamVR_TrackedObject>().index = (SteamVR_TrackedObject.EIndex)index;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
